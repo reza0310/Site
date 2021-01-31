b9013cd692e313c8dd6cb01e2a9ed906ae550860
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-01-31 23:32:46 +0100

    essai

---
+++ __init__.py
@@ -0,0 +1,95 @@
+import os
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.root_path, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/projet")
+def projet():
+    return render_template("projet.html")
+
+@app.route("/univers")
+def univers():
+    return render_template("univers.html")
+
+@app.route("/univers/aube")
+def aube():
+    return render_template("univers/aube.html")
+
+@app.route("/univers/aube/thulium")
+def thulium():
+    return render_template("univers/aube/Thulium.html")
+
+@app.route("/univers/aube/francium")
+def francium():
+    return render_template("univers/aube/Francium.html")
+
+@app.route("/univers/aube/hydrogene")
+def hydrogene():
+    return render_template("univers/aube/Hydrogene.html")
+
+@app.route("/univers/autre")
+def autre():
+    return render_template("univers/autre.html")
+
+@app.route("/actualites")
+def actualites():
+    return render_template("actualites.html")
+
+@app.route("/clubs")
+def clubs():
+    return render_template("clubs.html")
+
+@app.route("/telechargements")
+def telechargements():
+    return render_template("telechargements.html")
+
+@app.route("/outils")
+def contact():
+    return render_template("outils.html")
+
+@app.route('/calcul_imc')
+def calcul_imc():
+    return send_from_directory(app.root_path, 'JavaScripts/calcul_imc.js', mimetype='JavaScript')
+
+@app.route("/credits")
+def credits():
+    return render_template("credits.html")
+
+posts = [
+    {'auteur': 'reza0310', 'titre': 'Premier post',
+     'contenu': 'Bienvenue sur le blog de l\'O.D.A.A.M.E. qui est plus là en tant que démo technique qu\'autre chose.',
+     'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'},
+    {'auteur': 'reza0310', 'titre': 'Lorem ipsum', 'contenu': 'Lorem ipsum dolor sit amet', 'date': '14/11/2020'}
+]
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", posts=posts, titre="blog")
+
+if __name__ == "__main__":
+    app.run()
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="SiteWeb",
+    version="0.0.1",
+    author="reza0310",
+    author_email="reza031077@gmail.com",
+    description="Site Web",
+    long_description="Mon site web pour un premier deploiement",
+    url="https://github.com/reza0310/Site",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
